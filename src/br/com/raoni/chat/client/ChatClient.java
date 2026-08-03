813d46772f03b77b82cc354dc8abb4dc9cbc518b
Raoni Rodrigues <raonirodriguesdesousa11@gmail.com>
2026-08-03 09:28:04 -0300

    initial test of connection with the server

--- src/br/com/raoni/chat/client/ChatClient.java
+++ src/br/com/raoni/chat/client/ChatClient.java
@@ -1,4 +1,28 @@
 package br.com.raoni.chat.client;
 
+import javax.swing.*;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
 public class ChatClient {
+    public static void main(String[] args) {
+
+        try{
+            Socket client = new Socket("Raoni", 12345);
+            ObjectInputStream input = new ObjectInputStream(client.getInputStream());
+            String hello = (String) input.readObject();
+            JOptionPane.showMessageDialog(null, "messagem:"+ hello);
+            input.close();
+
+
+
+        }catch (Exception e){
+            System.out.println("erro:" + e.getMessage());
+        }
+
+
+    }
+
+
+
 }
--- src/br/com/raoni/chat/server/ChatServer.java
+++ src/br/com/raoni/chat/server/ChatServer.java
@@ -1,4 +1,27 @@
 package br.com.raoni.chat.server;
 
+import java.io.ObjectOutputStream;
+import java.net.*;
+
 public class ChatServer {
+   public static void main(String[] args){
+       try {
+           ServerSocket server = new ServerSocket(12345);
+           System.out.println("servidor escutando na porta 12345");
+
+           while (true) {
+               Socket client = server.accept();
+               System.out.println("Cliente conectado: " + client.getInetAddress().getHostAddress());
+               ObjectOutputStream output = new ObjectOutputStream(client.getOutputStream());
+               output.flush();
+               output.writeObject("Hello World");
+               output.close();
+               client.close();
+           }
+
+       }catch (Exception e){
+           System.out.println("erro: " + e.getMessage());
+       }
+   }
+
 }
